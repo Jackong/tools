c737c01a590bf8a32a21562177b55da67eba232d
Jackong <JackongC@gmail.com>
2014-06-05 09:45:03 +0800

    add require for frontend

--- view/js/app/services/account.js
+++ view/js/app/services/account.js
@@ -1,12 +1,15 @@
 /**
  * Created by daisy on 14-6-4.
  */
-angular.module('accountService', ['ngResource'])
-.factory('Account', ['$resource',
-    function($resource){
-        return $resource('accounts', {}, {
-            query: {
-                method:'GET', isArray:true
-            }
-        });
-}]);
+define(['lib/angular.min', 'lib/angular-resource.min'], function() {
+    var account = angular.module('accountService', ['ngResource']);
+    account.factory('Account', ['$resource',
+        function($resource){
+            return $resource('accounts', {}, {
+                query: {
+                    method:'GET', isArray:true
+                }
+            });
+        }]);
+    return account;
+});
