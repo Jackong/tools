0246c4e3640d4c141ef8d0a3b55fd3bf590fc510
zhemdaisy <zhemdaisy@gmail.com>
2014-07-12 12:54:39 +0800

    force umount and mount log

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ vm=/home/docker/app
 host=/Users/daisy/Project/node/iwomen
 
 echo "destroy env"
-umount $host
+umount -f $host
 docker ps | grep $container | awk '{print $1}' | xargs docker stop >/dev/null 2>&1
 docker ps -a | grep "Exited" | awk '{print $1}' | xargs docker rm >/dev/null 2>&1
 docker images -a | grep "<none>" | awk '{print $3}' | xargs docker rmi >/dev/null 2>&1
@@ -17,7 +17,7 @@ echo "init container-vm-host mount dir"
 boot2docker ssh "[ ! -d $vm ] && mkdir $vm && touch $vm/server.js"
 
 echo "run $container"
-ps=$(docker run -p 80:18080 --name iwomen-web -d -v $vm:$cont $container)
+ps=$(docker run -p 80:18080 --name iwomen-web -d -v $vm:$cont -v $vm/log:/var/log/node $container)
 
 echo "cp app from container to vm"
 boot2docker ssh "docker cp $ps:$cont /home/docker/"
